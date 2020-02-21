e2c2e0db35239cf8753e63d24da4849c203a70a2
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-02-21 11:31:33 +0200

    test card button

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,6 +2,9 @@ console.log("hello world! trello power up")
 
 window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
-        return [];
+        return t.card('all')
+            .then(function (card) {
+                console.log(JSON.stringify(card, null, 2));
+            });
     }
 })

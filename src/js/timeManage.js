3f78bc88358cc3a3c0005555668f409c535a8fe6
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-05-05 14:18:58 +0300

    totalSPent

--- src/js/timeManage.js
+++ src/js/timeManage.js
@@ -1,17 +1,31 @@
 var t = TrelloPowerUp.iframe();
 
+function addTimeToTotalSPent(value){
+    return new Promise((resolve) => {
+        var curentTotal;
+        t.get('card', 'shared', 'timeSpentToAdd').then(function(data) {
+            curentTotal = data;
+            var newTotal = value + curentTotal;
+            t.set('card', 'shared', 'timeSpent', newTotal).then(function(){
+                resolve();
+            });
+        });
+    })
+}
+
 document.getElementById('closePopup').onclick = function(){
     t.closePopup();
 }
 document.getElementById('insertValue').onclick = function(){
-    t.set('card', 'shared', 'timeSpent', document.getElementById('timeSpent').value).then(function(){
-        t.closePopup();
+    addTimeToTotalSPent(document.getElementById('timeSpentToAdd').value).then(function(){
+        t.popupClose();
     });
+    
 }
 
 t.render(function() {
-    t.get('card', 'shared', 'timeSpent').then(function(data) {
-        document.getElementById('timeSpent').value = data ? data : "";
+    t.get('card', 'shared', 'timeSpentToAdd').then(function(data) {
+        document.getElementById('timeSpentToAdd').value = data ? data : "";
     });
 })
 

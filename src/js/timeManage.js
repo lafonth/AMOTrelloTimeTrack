9157a75692e9652ab17c08d287cd03f983360fad
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-05-05 14:42:38 +0300

    time display

--- src/js/timeManage.js
+++ src/js/timeManage.js
@@ -3,10 +3,10 @@ var t = TrelloPowerUp.iframe();
 function addTimeToTotalSPent(value){
     return new Promise((resolve) => {
         var curentTotal;
-        t.get('card', 'shared', 'timeSpentToAdd').then(function(data) {
-            curentTotal = data;
-            var newTotal = value + curentTotal;
-            t.set('card', 'shared', 'timeSpent', newTotal).then(function(){
+        t.get('card', 'shared', 'timeTrack').then(function(data) {
+            curentTotal = data.timeSpentToAdd;
+            data.timeSpentToAdd = value + curentTotal;
+            t.set('card', 'shared', 'timeTrack', data).then(function(){
                 resolve();
             });
         });
@@ -24,8 +24,19 @@ document.getElementById('insertValue').onclick = function(){
 }
 
 t.render(function() {
-    t.get('card', 'shared', 'timeSpentToAdd').then(function(data) {
-        document.getElementById('timeSpentToAdd').value = data ? data : "";
+    t.get('card', 'shared', 'timeTrack').then(function(data) {
+        var container = document.getElementById('totalSpent');
+        if(data.timeSpent){
+            var time = data.timeSpent ? data.timeSpent : "";
+            container.textContent = "You passed ";
+            var timeElem = document.createElement('span')
+            timeElem.textContent = time;
+            container.append(timeElem);
+            container.textContent = " on this task.";
+        }else{
+            container.textContent = "You didn't set time on this task yet."
+        }
+        
     });
 })
 

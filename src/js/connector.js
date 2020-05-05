51d771d2b100499c8d183ee87a69bbc722287852
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-05-05 14:08:51 +0300

    size test

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,10 +2,8 @@ var manageTimeButton = function (t, opts) {
     console.log(t);
     return t.popup({
         title: 'Time spent',
-        url: './timeManage.html'
-        // args: {
-        //     name: t.get('member')
-        // }
+        url: './timeManage.html',
+        height: 80
     });
 }
 

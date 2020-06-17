286e41de9617d9b032e911fbeb2f99d360c9256c
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-06-17 18:09:39 +0300

    fix 2

--- src/js/timeManage.js
+++ src/js/timeManage.js
@@ -65,18 +65,18 @@ document.getElementById('insertValue').onclick = function () {
 /////render/////
 t.render(function () {
     console.log("render triggered");
-    // var totalContainer = document.getElementById('totalTimeSpent');
-    // var time = calculTotalTimeSpent();
-    // if (time > 0) {
-    //     totalContainer.textContent = "You passed ";
-    //     var timeElem = document.createElement('span')
-    //     timeElem.textContent = time;
-    //     totalContainer.append(timeElem);
-    //     totalContainer.textContent = " on this task.";
-    // } else {
-    //     totalContainer.textContent = "You didn't set time spent on this task yet."
-    // }
+    var totalContainer = document.getElementById('totalTimeSpent');
+    var time = calculTotalTimeSpent();
+    if (time > 0) {
+        totalContainer.textContent = "You passed ";
+        var timeElem = document.createElement('span')
+        timeElem.textContent = time;
+        totalContainer.append(timeElem);
+        totalContainer.textContent = " on this task.";
+    } else {
+        totalContainer.textContent = "You didn't set time spent on this task yet."
+    }
 
-    // var logsContainer = document.getElementById('logTimeSpent');
-    // logsContainer.textContent = displayLogs();
+    var logsContainer = document.getElementById('logTimeSpent');
+    logsContainer.textContent = displayLogs();
 })

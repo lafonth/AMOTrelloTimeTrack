3597ef6764d85f2143c1c7547dfa13fed18653ca
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-06-17 17:06:14 +0300

    another bug

--- src/js/timeManage.js
+++ src/js/timeManage.js
@@ -66,18 +66,18 @@ document.getElementById('insertValue').onclick = function () {
 /////render/////
 t.render(function () {
     console.log("render triggered");
-    var totalContainer = document.getElementById('totalTimeSpent');
-    calculTotalTimeSpent().then(function (time) {
-        if (time) {
-            totalContainer.textContent = "You passed ";
-            var timeElem = document.createElement('span')
-            timeElem.textContent = time;
-            totalContainer.append(timeElem);
-            totalContainer.textContent = " on this task.";
-        } else {
-            totalContainer.textContent = "You didn't set time spent on this task yet."
-        }
-    });
+    // var totalContainer = document.getElementById('totalTimeSpent');
+    // calculTotalTimeSpent().then(function (time) {
+    //     if (time) {
+    //         totalContainer.textContent = "You passed ";
+    //         var timeElem = document.createElement('span')
+    //         timeElem.textContent = time;
+    //         totalContainer.append(timeElem);
+    //         totalContainer.textContent = " on this task.";
+    //     } else {
+    //         totalContainer.textContent = "You didn't set time spent on this task yet."
+    //     }
+    // });
 
     // var logsContainer = document.getElementById('logTimeSpent');
     // logsContainer.textContent = displayLogs();

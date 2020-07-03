3500b51b0d8b47514e008ab0a1734d2dc4825b5a
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-07-03 14:47:31 +0300

    test

--- src/js/timeManage.js
+++ src/js/timeManage.js
@@ -76,14 +76,15 @@ function updateDisplay() {
 
 function displayLogs() {
     t.get('card', 'shared', 'timeTrack').then(function (data) {
-        console.log(data);
+        
         if (typeof data == 'undefined') {
             data = {
                 logs: new Array
             }
         }
         data.logs.forEach(log => {
-            $('#logTimeSpent .tbody').append(`<tr>
+            console.log($('#logTimeSpent > tbody'));
+            $('#logTimeSpent > tbody').append(`<tr>
                     <td>` +
                 "log.member" +
                 `</td>

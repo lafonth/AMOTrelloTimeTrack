d372003a647d8e4aa9bfdd9e36322b57894e506c
hubert.lafont <hubert.lafont@SVROLP00889.ro.softvision.global>
2020-08-24 12:24:03 +0300

    beginning merge with main power up

---
+++ src/js/amo.js
@@ -0,0 +1,58 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Portions Copyright (C) Philipp Kewisch, 2019 */
+
+const API_PREFIX = "https://addons.mozilla.org/api/v4/addons/addon/";
+const ADDON_LINKS_RE = /https:\/\/(?:reviewers\.)?(addons\.mozilla\.org|addons\.allizom\.org|addons-dev\.allizom\.org|addons\.thunderbird\.net)\/([^/]*)\/(reviewers\/review(|-listed|-unlisted|-content)|admin\/addon\/manage|[^/]*\/addon|developers\/feed)\/([^/#?]*)(\/edit)?/;
+
+var t = window.TrelloPowerUp.iframe();
+
+t.render(async () => {
+  let attachments = await t.card("attachments").get("attachments");
+  let slugs = attachments.reduce((acc, attachment) => {
+    let match = attachment.url.match(ADDON_LINKS_RE);
+    if (match) {
+      acc.push(match[5]);
+    }
+    return acc;
+  }, []);
+
+  document.querySelector("#content").innerHTML = "";
+  document.querySelector("#loading").classList.add("loading");
+
+  let info = slugs.map((slug) => fetch(API_PREFIX + slug).then(async (resp) => {
+    let data = await resp.json();
+    if (!data.url) {
+      let parts = resp.url.split("/");
+      data.slug = parts[parts.length - 1] || parts[parts.length - 2];
+      data.url = "https://addons.mozilla.org/en-US/firefox/addon/" + data.slug;
+      data.review_url = "https://reviewers.addons.mozilla.org/en-US/reviewers/review/" + data.slug;
+    }
+    return data;
+  }));
+
+  let results = await Promise.all(info);
+  document.querySelector("#loading").classList.remove("loading");
+
+  for (let addon of results) {
+    try {
+      console.log(addon);
+      let template = document.querySelector("#addon");
+      let clone = document.importNode(template.content, true);
+
+      let name = addon.name ? addon.name["en-US"] || addon.name[addon.default_locale] || "unknown" : addon.slug;
+
+      clone.querySelector(".name").textContent = name;
+      clone.querySelector(".img").src = addon.icon_url || "../images/addon.png";
+      clone.querySelector(".url.listing").setAttribute("href", addon.url);
+      clone.querySelector(".url.review").setAttribute("href", addon.review_url || "");
+
+      document.querySelector("#content").appendChild(clone);
+    } catch (e) {
+      console.error(e, addon);
+    }
+  }
+
+  return t.sizeTo("#content");
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,85 @@
 import 'bootstrap/dist/css/bootstrap.css';
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Portions Copyright (C) Philipp Kewisch, 2019 */
+
+const ADDON_LINKS_RE = /https:\/\/(?:reviewers\.)?(addons\.mozilla\.org|addons\.allizom\.org|addons-dev\.allizom\.org|addons\.thunderbird\.net)\/([^/]*)\/(reviewers\/review(|-listed|-unlisted|-content)|admin\/addon\/manage|[^/]*\/addon|developers\/feed)\/([^/#?]*)(\/edit)?/;
+
+var manageTimeButton = function (t, opts) {
+  t.card('members').then(function (members) {
+      return t.modal({
+          title: 'Time spent',
+          url: '../html/timeManage.html',
+          height: 400,
+          args: {
+              members: members
+          }
+      });
+  });
+}
+
+/**
+ * Begin trello powerup initialization.
+ */
+TrelloPowerUp.initialize({
+  "attachment-sections": function (t, opts) {
+    let claimed = opts.entries.filter(attachment => attachment.url.match(ADDON_LINKS_RE));
+    if (!claimed.length) {
+      return [];
+    }
+
+    return [{
+      title: "Reviews",
+      claimed: claimed,
+      icon: TrelloPowerUp.util.relativeUrl("./images/addon.png"),
+      content: {
+        type: "iframe",
+        url: t.signUrl("../html/amo.html"),
+        height: 64
+      }
+    }];
+  },
+
+  "card-from-url": async function (t, opts) {
+    let match = opts.url.match(ADDON_LINKS_RE);
+
+    try {
+      let resp = await fetch("https://addons.mozilla.org/api/v4/addons/addon/" + match[5]);
+      let data = await resp.json();
+
+      return {
+        name: "Review: " + data.name["en-US"],
+      };
+    } catch (e) {
+      console.error(e);
+      throw t.NotHandled();
+    }
+  },
+
+  "list-actions": function (t, opts) {
+    return [{
+      text: "Open all reviews in tabs",
+      callback: async function (t) {
+        let cards = await t.cards("attachments");
+        for (let card of cards) {
+          let attachment = card.attachments.find(attach => attach.url.match(ADDON_LINKS_RE));
+          if (attachment) {
+            window.open(attachment.url, "_blank");
+          }
+        }
+      }
+    }];
+  },
+  "format-url": function (t, opts) {
+    throw t.NotHandled();
+  },
+  'card-buttons': function (t, opts) {
+    return [{
+      text: 'Time Track',
+      callback: manageTimeButton
+    }];
+  }
+}, {
+  appName: "AMO Reviewers Trello Companion"
+});
